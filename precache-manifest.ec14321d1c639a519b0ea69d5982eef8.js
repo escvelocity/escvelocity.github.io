3045298ee94e7e6d78f3cc10d98e011803fa55d2
Slevin Zhang <s7evinz@gmail.com>
2019-08-19 21:52:19 -0400

    Initial build

---
+++ precache-manifest.ec14321d1c639a519b0ea69d5982eef8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ff553caa6b1c67e02f332abd6b5eb21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56420257029ee3beb95d",
+    "url": "/static/css/2.62908225.chunk.css"
+  },
+  {
+    "revision": "30f38992434aa4f52690",
+    "url": "/static/css/main.7f8b46dc.chunk.css"
+  },
+  {
+    "revision": "56420257029ee3beb95d",
+    "url": "/static/js/2.c985406a.chunk.js"
+  },
+  {
+    "revision": "30f38992434aa4f52690",
+    "url": "/static/js/main.c1a4979e.chunk.js"
+  },
+  {
+    "revision": "02c5e08da9ddc8616fd6",
+    "url": "/static/js/runtime~main.3ff6abe0.js"
+  },
+  {
+    "revision": "e1a84b66bae01c4b0aac415b44aa7780",
+    "url": "/static/media/Dice-0.e1a84b66.svg"
+  },
+  {
+    "revision": "1192cb54c3102251886574143c88f11f",
+    "url": "/static/media/Dice-1.1192cb54.svg"
+  },
+  {
+    "revision": "cb855bd35548238cd613ee129517d9f2",
+    "url": "/static/media/Dice-2-b.cb855bd3.svg"
+  },
+  {
+    "revision": "6e06b439e225c6d141e6fa25c13c56b8",
+    "url": "/static/media/Dice-3-b.6e06b439.svg"
+  },
+  {
+    "revision": "5f9fb5bd10964d1307c53cfe6b1caa7f",
+    "url": "/static/media/Dice-4.5f9fb5bd.svg"
+  },
+  {
+    "revision": "be6d963e2d0eeab743ac8472d2be1f24",
+    "url": "/static/media/Dice-5-b.be6d963e.svg"
+  },
+  {
+    "revision": "9b3f333ac29647fcbcdfda86a7c2e8ca",
+    "url": "/static/media/Dice-6-b.9b3f333a.svg"
+  }
+]);
